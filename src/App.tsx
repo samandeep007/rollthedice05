96325a5b173be526a659287406c739153461384f
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-03 22:41:04 -0600

    refactor: Add image import and styles to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import DiceOne from '../assets/One.png'
 
-export default function App() {
+export default function App(): JSX.Element {
   return (
     <View>
       <Text>App</Text>
@@ -9,4 +10,29 @@ export default function App() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
